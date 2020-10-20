caff5f9023a9395c71c7dc956990fd225b7cd942
TremblingOdin <pigmekevin@gmail.com>
2020-10-19 20:59:35 -0600

    Added comments and commit_stmemory function

--- src/memory.py
+++ src/memory.py
@@ -6,7 +6,17 @@ import config
 class Memory:
     def __init__(self, MEMORY_SIZE):
         self.MEMORY_SIZE = config.MEMORY_SIZE
+        # long term memory
         self.ltmemory = deque(maxlen=config.MEMORY_SIZE)
+        # short term memory
         self.stmemory = deque(maxlen=config.MEMORY_SIZE)
 
-
+    def commit_stmemroy(self, identities, state, action_values):
+        for r in identities(state, action_values):
+            self.stmemory.append({
+                'board': r[0].board
+                , 'state': r[0]
+                , 'id': r[0].id
+                , 'AV': r[1]
+                , 'playerTurn': r[0].player_turn
+                })

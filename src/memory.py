da6049e8a5471addfb6a64cc87f3d5fd6fa2bf28
TremblingOdin <pigmekevin@gmail.com>
2020-10-19 20:56:57 -0600

    created the init function for Memory class

--- src/memory.py
+++ src/memory.py
@@ -3,4 +3,10 @@ from collections import deque
 
 import config
 
+class Memory:
+    def __init__(self, MEMORY_SIZE):
+        self.MEMORY_SIZE = config.MEMORY_SIZE
+        self.ltmemory = deque(maxlen=config.MEMORY_SIZE)
+        self.stmemory = deque(maxlen=config.MEMORY_SIZE)
+
 

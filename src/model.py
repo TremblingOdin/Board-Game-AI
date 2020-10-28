747b8b3efc0249ac8040ab4e4aea9f06f9fa11ec
TremblingOdin <pigmekevin@gmail.com>
2020-10-27 23:03:46 -0600

    initialized General_Model

--- src/model.py
+++ src/model.py
@@ -11,3 +11,10 @@ from keras import regularizers
 import keras.backend as K
 
 from loss import softmax_cross_entropy_with_logits
+
+class General_Model():
+    def __init__(self, reg_const, learning_rate, input_dim, output_dim):
+        self.reg_const = reg_const
+        self.learning_rate = learning_rate
+        self.input_dim = input_dim
+        self.output_dim = output_dim

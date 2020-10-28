bfbf6ed37783664633f90d74eef8c0eff6d29cb1
TremblingOdin <pigmekevin@gmail.com>
2020-10-27 23:00:33 -0600

    added keras imports

--- src/model.py
+++ src/model.py
@@ -1,4 +1,13 @@
 import config
 import numpy as np
 
+import matplotlib.pyplot as plt
 
+from keras.models import Sequential, load_model, Model
+from keras.layers import Input, Dense, Conv2D, Flatten, BatchNormalization, Activation, LeakyReLU, add
+from keras.optimizers import SGD
+from keras import regularizers
+
+import keras.backend as K
+
+from loss import softmax_cross_entropy_with_logits

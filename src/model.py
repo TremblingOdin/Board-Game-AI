8a3484986667cf35fbf10b3ebb8fa5b3675ff26f
TremblingOdin <pigmekevin@gmail.com>
2020-10-28 23:28:37 -0600

    Added predict funciton

--- src/model.py
+++ src/model.py
@@ -18,3 +18,8 @@ class General_Model():
         self.learning_rate = learning_rate
         self.input_dim = input_dim
         self.output_dim = output_dim
+
+    def predict(self, x):
+        return self.model.predict(x)
+
+
